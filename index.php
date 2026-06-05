42bf060de00f3d858d90dd278477a3691abb7a24
TaHaiLam2k6 <nguyentunglam1726@gmail.com>
2026-06-05 21:44:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,24 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn = new mysqli("localhost", "webuser", "123456", "webdemo");
-if ($conn->connect_error) {
-die("Connection failed");
-}
-$message = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+header("Content-Type: application/json");
+
+$conn = new mysqli(
+    getenv("DB_HOST"),
+    getenv("DB_USER"),
+    getenv("DB_PASS"),
+    getenv("DB_NAME"),
+    getenv("DB_PORT")
+);
+
 $username = $_POST["username"];
 $password = $_POST["password"];
+
 $sql = "SELECT * FROM users
-WHERE username= '$username'
-AND password='$password'";
+        WHERE username='$username'
+        AND password='$password'";
+
 $result = $conn->query($sql);
-if ($result->num_rows > 0) {
-header("Location: index.html");
-exit();
-} else {
-$message = "Sai tai khoan hoac mat khau!";
-}
-}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Register</title>
-</head>
-<body>
-<h1>Register</h1>
-<form method="POST">
-<input type="text"
-name ="username"
-placeholder="Username">
-<br><br>
-<input type="password"
-name="password"
-placeholder="Password">
-<br><br>
-<button type="submit">Register</button>
-</form>
-</body>
-</html>
+
+echo json_encode([
+    "success" => $result->num_rows > 0
+]);
